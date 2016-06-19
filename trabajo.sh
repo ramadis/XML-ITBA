27d9e325bbc3fa61a52e9c509a3791792a3a7f41
Rama Olivera Fedi <rramiro.o@hotmail.com>
2016-06-19 19:24:38 -0300

    improved code

--- trabajo.sh
+++ trabajo.sh
@@ -1,8 +1,7 @@
 if [[ $1 ]]
 then
   java net.sf.saxon.Query junta.xq fecha=$1 > datos_json.xml
+  java net.sf.saxon.Transform -s:datos_json.xml -xsl:eventos.xslt > eventos.json
 else
   echo "Debe ingresar la fecha por argumento"
 fi
-
-java net.sf.saxon.Transform -s:datos_json.xml -xsl:eventos.xslt > eventos.json
